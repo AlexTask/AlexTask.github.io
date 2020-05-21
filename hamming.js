0aa4dbed0c509e97b3feedabd2da6d0a51a50909
AlexTask <tyshchenko.alex@gmail.com>
2020-05-21 17:21:28 +0300

    'stars'

--- hamming.js
+++ hamming.js
@@ -28,6 +28,8 @@ function hammingEncode(input) {
         output = output.slice(0, key - 1) + '0' + output.slice(key - 1);
     }
 
+    var nullControl = output;
+
     debug && console.log('With control bits: ' + output);
 
     //Calculate control bits
@@ -59,7 +61,80 @@ function hammingEncode(input) {
 
     debug && console.log('Encoding ouput: ' + output);
 
+    
+    
+    showEncodeInfo(arr,controlBitsIndexes, nullControl.split(''), input);
+
     return output;
+}
+
+function showEncodeInfo(arr, controlBitsIndexes, inputArr, input) {
+    $('.js-str-l').text(input.length);
+    $('.js-control-bits-nums').text(controlBitsIndexes.join(', '));
+
+    var table = '<table cellspacing="0" class="enc-table"><tr>';
+
+    //Add new empty control bits
+    for (i = 0; i < inputArr.length; i++) {
+        var addClass = '';
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            addClass = 'class="control-bit"';
+        }
+
+        table += '<th ' + addClass +'>' + inputArr[i] + '</th>';
+    }
+
+    table += '<th>біт</th><th>сума</th><th>значення</th></tr>';
+
+    //Show bits calculations
+    for (i = 0; i < controlBitsIndexes.length; i++) {
+        table += '<tr>';
+        var bitKey = controlBitsIndexes[i];
+
+        var controlBit = 0;
+        var controlBitVal = 0;
+        var corrBits = [];
+        for (j = (bitKey - 1); j < inputArr.length; j += (2 * bitKey)) {
+            for (m = 0; m < bitKey && j + m < inputArr.length; m++) {
+                corrBits.push(j + m);
+                //table += '<td class="control-bit">' + arr[j + m] + '</td>';
+                //controlBits.push(arr[j + m]);
+                controlBit += parseInt(inputArr[j + m]);
+                controlBitVal = (controlBit % 2);
+            }
+        }
+
+        for (let j = 0; j < arr.length; j++) {
+            if (corrBits.indexOf(j) > -1) {
+                table += '<td>' + arr[j] + '</td>';
+            } else {
+                table += '<td></td>';
+            }
+        }
+
+        
+
+        table += '<td class="add-info">' + bitKey + '</td><td class="add-info">' + controlBit + '</td><td class="add-info">' + controlBitVal + '</td></tr>';
+    }
+
+
+    table += '</tr>';
+
+    table += '</table>';
+    $('.js-encode-process').html(table);
+
+
+    var str = '';
+
+    for (i = 0; i < arr.length; i++) {
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            str += '<span class="control-bit">'+arr[i]+'</span>';
+        } else {
+            str += arr[i];
+        }
+    }
+
+    $('.js-control-bits').html(str);
 }
 
 //Decode without correction
@@ -77,9 +152,12 @@ function hammingPureDecode(input) {
 
     i = 1;
     while (l / i >= 1) {
+        console.log('l / i', (l / i));
         controlBitsIndexes.push(i);
         i *= 2;
     }
+
+    console.log('controlBitsIndexes', controlBitsIndexes);
 
     controlBitsIndexes.forEach(function (key, index) {
         originCode =
@@ -101,10 +179,11 @@ function hammingDecode(input) {
 
     var controlBitsIndexes = [];
     var sum = 0;
-    var l = input.length;
     var i = 1;
     var output = hammingPureDecode(input);
     var inputFixed = hammingEncode(output);
+
+    var l = output.length;
 
     debug && console.log('input: ' + input, 'inputFixed: ' + inputFixed);
 
@@ -139,7 +218,120 @@ function hammingDecode(input) {
 
     var fixedEncodedStr = inputArr.join('');
 
+    showDecode(input.split(''), inputFixed.split(''), fixedEncodedStr.split(''));
+
     return hammingPureDecode(fixedEncodedStr);
+}
+
+function showDecode(inArr, newArr, resArr) {
+    var table = '<table cellspacing="0" class="enc-table"><tr>';
+    console.log(inArr);
+    //console.log(hammingPureDecode(inArr));
+    var controlBitsIndexes = getControlBits(hammingPureDecode(inArr.join('')));
+
+    for (i = 0; i < inArr.length; i++) {
+        table += '<th>' + (i + 1) + '</th>';
+    }
+
+    table += '</tr><tr>';
+
+    var sumBits = [];
+    for (i = 0; i < inArr.length; i++) {
+        var addClass = '';
+
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            addClass = 'class="control-bit';
+            if (inArr[i] !== newArr[i]) {
+                addClass += ' check-bit';
+                sumBits.push(i+1);
+            }
+            addClass += '"';
+        }
+
+        table += '<th ' + addClass +'>' + inArr[i] + '</th>';
+    }
+
+    table += '</tr><tr>';
+
+    for (i = 0; i < newArr.length; i++) {
+        var addClass = '';
+
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            addClass = 'class="control-bit';
+            if (inArr[i] !== newArr[i]) {
+                addClass += ' check-bit';
+            }
+            addClass += '"';
+        }
+
+        table += '<th ' + addClass +'>' + newArr[i] + '</th>';
+    }
+
+    table += '</tr></table>';
+
+    $('.js-calc-bits').html(table);
+
+    
+    var str = '';
+    var sum = 0;
+    if (sumBits.length > 0) {
+        str += 'Рахуємо позицію невірного біту: '
+        for (i = 0; i < sumBits.length; i++) {
+            if (i == 0) {
+                str += sumBits[i];
+            } else {
+                str += ' + ' + sumBits[i];
+            }
+
+            sum = sum + parseInt(sumBits[i]);
+        }
+
+        str += ' = ' + sum + ' та заміяємо його на протилежний.';
+    }
+
+    $('.js-sum-control-bits').html(str);
+
+
+    //Result table
+    var resTable = '<table cellspacing="0" class="enc-table">';
+
+    for (i = 0; i < inArr.length; i++) {
+        resTable += '<th>' + (i + 1) + '</th>';
+    }
+
+    resTable += '</tr><tr>';
+
+    for (i = 0; i < resArr.length; i++) {
+        var addClass = 'class="';
+
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            addClass = 'control-bit';
+        }
+
+        if ((i + 1) == sum) {
+            addClass += 'res-bit check-bit';
+        }
+        addClass += '"';
+        resTable += '<th ' + addClass +'>' + resArr[i] + '</th>';
+    }
+
+    resTable += '</tr></table>';
+
+    $('.js-bits-decode-res').html(resTable);
+}
+
+function getControlBits(input) {
+    var controlBitsIndexes = [];
+    var l = input.length;
+    var i;
+
+    i = 1;
+    while (l / i >= 1) {
+        controlBitsIndexes.push(i);
+        i *= 2;
+    }
+
+    return controlBitsIndexes;
 }
 
 function hammingCheck(input) {
@@ -148,11 +340,53 @@ function hammingCheck(input) {
             'hamming-code error: input should be binary string, for example "101010"'
         );
     }
+    console.log(hammingPureDecode(input));
 
     var inputFixed = hammingEncode(hammingPureDecode(input));
 
+    showCheckProcess(input, inputFixed);
+
+    if (inputFixed === input) {
+        $('.js-no-errors').show();
+        $('.js-has-errors').hide();
+    } else {
+        $('.js-no-errors').hide();
+        $('.js-has-errors').show();
+    }
+
     return !(inputFixed === input);
 }
+
+function showCheckProcess(inArr, checkArr) {
+    var table = '<table cellspacing="0" class="enc-table"><tr>';
+    var controlBitsIndexes = getControlBits(hammingPureDecode(inArr));
+    console.log(inArr.length, checkArr.length);
+    //Add new empty control bits
+    for (i = 0; i < inArr.length; i++) {
+        var addClass = '';
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            addClass = 'class="control-bit"';
+        }
+
+        table += '<th ' + addClass +'>' + inArr[i] + '</th>';
+    }
+
+    table += '</tr><tr>';
+
+    for (i = 0; i < checkArr.length; i++) {
+        var addClass = '';
+        if (controlBitsIndexes.indexOf(i+1) > -1) {
+            addClass = 'class="control-bit"';
+        }
+
+        table += '<th ' + addClass +'>' + checkArr[i] + '</th>';
+    }
+
+    table += '</tr></table>';
+
+    $('.js-check-process').html(table);
+}
+
 
 
 //------------------UI----------------------
@@ -167,7 +401,7 @@ $(".js-decode-btn").on("click", function () {
 
     $('.js-has-error').text(hammingCheck(code) ? 'Так' : 'Ні');
     $('.js-result-code').val(hammingDecode(code));
-    $('.js-result-noncorrect-code').val(hammingPureDecode(code));
+    //$('.js-result-noncorrect-code').val(hammingPureDecode(code));
 });
 
 $('.js-encoded-code').val('');
